6043544f5fbb4e3adba0dd26e9fbb34ed54c94bf
Leonhard Kuboschek <x32000@gmail.com>
2018-10-31 03:15:45 +0100

    Add empty social page. Refactor App component.

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Announcements from '@/components/Announcements'
+import Social from '@/components/Social'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Announcements',
+      name: 'News',
       component: Announcements
+    },
+    {
+      path: 'you',
+      name: 'You',
+      component: Social
     }
   ]
 })
